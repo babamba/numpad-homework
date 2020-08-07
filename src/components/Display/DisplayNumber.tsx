1fa4d43e1e8049ebdbfb3f662c975230e86b57dd
jw.kim <babamba88@gintlab.com>
2020-08-07 09:17:33 +0900

    perf: :triangular_flag_on_post: add polyfill

--- src/components/Display/DisplayNumber.tsx
+++ src/components/Display/DisplayNumber.tsx
@@ -1,7 +1,7 @@
-import React, { FC, useState, useEffect, useRef } from "react";
-import styled from "styled-components";
-import { observer } from "mobx-react";
-import useStores from "../../hooks/useStores";
+import React, { FC, useState, useEffect, useRef } from 'react';
+import styled from 'styled-components';
+import { observer } from 'mobx-react';
+import useStores from '../../hooks/useStores';
 
 const Conatiner = styled.div`
   padding: 41px 48px;
@@ -10,7 +10,7 @@ const Conatiner = styled.div`
   align-items: center;
 `;
 const DisplayContent = styled.span`
-  font-family: "SpoqaHanSans-Regular";
+  font-family: 'SpoqaHanSans-Regular';
   font-size: 56px;
   color: #4d5256;
   letter-spacing: 0;
@@ -20,13 +20,13 @@ const DisplayContent = styled.span`
 
 const DisplayNumber: FC = () => {
   const {
-    numpadStore: { hypenNumber, useMaskingMode },
+    numpadStore: { hypenNumber, useMaskingMode }
   } = useStores();
 
   const [displayNum, setDisplayNum] = useState(hypenNumber);
 
   const masking = (hypenNumber: string) => {
-    let strArr = hypenNumber.split("-");
+    let strArr = hypenNumber.split('-');
 
     // 010 이후로 번호가 있다면.
     if (strArr.length > 1) {
@@ -36,17 +36,17 @@ const DisplayNumber: FC = () => {
           return item;
         } else {
           if (item.length > 0) {
-            let replace = "";
+            let replace = '';
             for (const str of item) {
               // 숫자 개수만큼 *처리
-              replace = replace + "*";
+              replace = replace + '*';
             }
             return replace;
           }
         }
       });
       // 하이픈 구분자 첨부하여 다시 하나로 합치고 리턴
-      setDisplayNum(replaceArr.join("-"));
+      setDisplayNum(replaceArr.join('-'));
     } else {
       // 010 앞부분 밖에 번호가 없을땐 그대로 리턴
       setDisplayNum(hypenNumber);
@@ -54,8 +54,8 @@ const DisplayNumber: FC = () => {
   };
 
   useEffect(() => {
-    const current = displayNum.split("-").join("").length; // 변경된 값
-    const next = hypenNumber.split("-").join("").length; // 현재화면 노출값
+    const current = displayNum.split('-').join('').length; // 변경된 값
+    const next = hypenNumber.split('-').join('').length; // 현재화면 노출값
 
     if (useMaskingMode) {
       // 마스킹 모드면
@@ -74,10 +74,6 @@ const DisplayNumber: FC = () => {
       //그대로 노출
       setDisplayNum(hypenNumber);
     }
-
-    return () => {
-      console.log("clear");
-    };
   }, [hypenNumber]);
 
   useEffect(() => {
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import { Provider } from "mobx-react";
-import "mobx-react-lite/batchingForReactDom";
-import * as serviceWorker from "./serviceWorker";
-import GlobalStyle from "./styles/global-styles";
-import { BrowserRouter } from "react-router-dom";
-import RootStore from "./stores/index";
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
+import 'proxy-polyfill';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { Provider } from 'mobx-react';
+import 'mobx-react-lite/batchingForReactDom';
+import * as serviceWorker from './serviceWorker';
+import GlobalStyle from './styles/global-styles';
+import { BrowserRouter } from 'react-router-dom';
+import RootStore from './stores/index';
 
 const rootStore = new RootStore(); // *** 루트 스토어 생성
 
@@ -19,7 +24,7 @@ ReactDOM.render(
       </Provider>
     </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
